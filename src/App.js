d4ce4cda178bcefe9196fbc2529ed01e676e9d46
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-20 20:02:17 -0400

    action, loaders and router files added to src folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Index from './pages/Index';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Index/>
     </div>
   );
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Form, useSubmit } from 'react-router-dom'
+
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div>
+        <h3>Add a New Bookmark</h3>
+        <Form action="/create" method='POST'>
+            <input type='text' name="name" placeholder='Website Name'/>
+            <input type='text' name="link" placeholder='http://'/>
+            <input type='submit' value='Add Bookmark!' />
+        </Form>
+    </div>
   )
 }
 
