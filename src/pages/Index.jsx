aad62a7ecb107cbb706458816676a90f2c58a111
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-22 15:38:39 -0400

    css added to index.js input fields

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -14,11 +14,44 @@ const Index = () => {
 
   return (
     <div>
-      <h3>Bookmark a Helpful Coding Website</h3>
+      <h3 style={{ textDecoration: 'underline', fontSize:'25px'}}>Bookmark a Helpful Coding Website</h3>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Website Name" value={formData.name} onChange={handleChange}/>
-        <input type="text" name="link" placeholder="http://" value={formData.link} onChange={handleChange}/>
-        <input type="submit" value="Add Bookmark!" />
+        <input
+          type="text" name="name" placeholder="Website Name" value={formData.name} onChange={handleChange}
+          style={{
+            width: '25%',
+            padding: '10px',
+            marginBottom: '15px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            fontSize: '16px',
+            color: '#333',
+          }}
+        />
+        <input
+          type="text" name="link" placeholder="http://" value={formData.link} onChange={handleChange}
+          style={{
+            width: '25%',
+            padding: '10px',
+            marginBottom: '15px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            fontSize: '16px',
+            color: '#333',
+          }}
+        />
+        <input
+          type="submit" value="Add Bookmark!"
+          style={{
+            width: '15%',
+            padding: '10px',
+            backgroundColor: '#71b8e4',
+            color: 'black',
+            border: 'none',
+            borderRadius: '5px',
+            fontSize: '16px',
+          }}
+        />
       </form>
     </div>
   );
