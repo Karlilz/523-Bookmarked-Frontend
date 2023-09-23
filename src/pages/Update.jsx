862106c447194cdf1b8ee3195f48e2e4610c6ac6
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-23 15:03:21 -0400

    update page working

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Update = () => {
+  const bookmark = useLoaderData();
+  const [formData, setFormData] = useState(bookmark);
+
+  const handleChange = (e) => {
+    setFormData(prevState => {
+        return {...prevState, [e.target.name] : e.target.value}
+    })
+  }
+
   return (
-    <div>Update</div>
-  )
+    <div>
+      <form action={`/update/${bookmark._id}`} method='post'>
+        <input type="text" name="title" value={formData.title} onChange={handleChange} placeholder='Enter Webiste'/>
+        <input type="text" name="url" value={formData.url} onChange={handleChange} placeholder='Enter URL'/>
+        {/* <input className="rounded-full p-2 bg-blue-200" type="submit" value={`Update ${people.name}`}/> */}
+      </form>
+    </div>
+  );
 }
 
-export default Update
+export default Update;
