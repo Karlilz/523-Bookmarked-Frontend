dc20f86b579666b3e02db0a6ed6d79066b38cb4b
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-23 15:47:43 -0400

    changes

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+// import React, { useState } from 'react';
+// import { useLoaderData } from 'react-router-dom';
 
-const Update = () => {
-  const bookmark = useLoaderData();
-  const [formData, setFormData] = useState(bookmark);
+// const Update = () => {
+//   const bookmark = useLoaderData();
+//   const [formData, setFormData] = useState(bookmark);
 
-  const handleChange = (e) => {
-    setFormData(prevState => {
-        return {...prevState, [e.target.name] : e.target.value}
-    })
-  }
+//   const handleChange = (e) => {
+//     setFormData(prevState => {
+//         return {...prevState, [e.target.name] : e.target.value}
+//     })
+//   }
 
-  return (
-    <div>
-      <form action={`/update/${bookmark._id}`} method='post'>
-        <input type="text" name="title" value={formData.title} onChange={handleChange} placeholder='Enter Webiste'/>
-        <input type="text" name="url" value={formData.url} onChange={handleChange} placeholder='Enter URL'/>
-        {/* <input className="rounded-full p-2 bg-blue-200" type="submit" value={`Update ${people.name}`}/> */}
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <form action={`/update/${bookmark._id}`} method='post'>
+//         <input type="text" name="title" value={formData.title} onChange={handleChange} placeholder='Enter Webiste'/>
+//         <input type="text" name="url" value={formData.url} onChange={handleChange} placeholder='Enter URL'/>
+//         <input className="rounded-full p-2 bg-blue-200" type="submit" value={`Update ${bookmark.title}`}/>
+//       </form>
+//     </div>
+//   );
+// }
 
-export default Update;
+// export default Update;
