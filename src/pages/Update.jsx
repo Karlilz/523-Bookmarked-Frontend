52cc430041422a29565dd9fc1c208743026f021e
Elliott Ubelhor <elliottubelhor@MacBook-Air.local>
2023-09-23 14:38:41 -0500

    Update Page

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const Update = () => {
+    const bookmark = useLoaderData()
+    const [formData, setFormData] = useState(bookmark);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    const { title, url } = event.target;
+    setFormData({ ...formData, [title]: value });
+  };
+
   return (
-    <div>Update</div>
+    <div>
+        <div>
+      <h3 style={{ textDecoration: 'underline', fontSize:'25px', fontStyle:'italic'}}>Bookmark a Helpful Coding Website</h3>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text" name="name" placeholder="Website Name" value={formData.name} onChange={handleChange}
+          style={{width: '15%', padding: '10px', marginBottom: '15px', border: '1px solid #ccc', borderRadius: '5px', fontSize: '16px', color: '#333'}}/>
+        <input
+          type="text" name="link" placeholder="http://" value={formData.link} onChange={handleChange}
+          style={{width: '15%', padding: '10px', marginBottom: '15px', border: '1px solid #ccc', borderRadius: '5px', fontSize: '16px', color: '#333'}}/>
+        <input
+          type="submit" value="Update Bookmark!"
+          style={{width: '15%', padding: '10px', backgroundColor: '#71b8e4', color: 'black', border: 'none', borderRadius: '5px', fontSize: '16px'}}/>
+      </Form>
+    </div>
+    </div>
   )
 }
 
