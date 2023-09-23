2eee9d80d31ef542347c93593546d5c1c31c048a
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-23 11:00:58 -0400

    imported Form to index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Form } from 'react-router-dom';
 
 const Index = () => {
   const [formData, setFormData] = useState({ name: '', link: '' });
@@ -14,20 +15,11 @@ const Index = () => {
 
   return (
     <div>
-      <h3 style={{ textDecoration: 'underline', fontSize:'25px'}}>Bookmark a Helpful Coding Website</h3>
-      <form onSubmit={handleSubmit}>
+      <h3 style={{ textDecoration: 'underline', fontSize:'25px', fontStyle:'italic'}}>Bookmark a Helpful Coding Website</h3>
+      <Form onSubmit={handleSubmit}>
         <input
           type="text" name="name" placeholder="Website Name" value={formData.name} onChange={handleChange}
-          style={{
-            width: '15%',
-            padding: '10px',
-            marginBottom: '15px',
-            border: '1px solid #ccc',
-            borderRadius: '5px',
-            fontSize: '16px',
-            color: '#333',
-          }}
-        />
+          style={{width: '15%', padding: '10px', marginBottom: '15px', border: '1px solid #ccc', borderRadius: '5px', fontSize: '16px', color: '#333'}}/>
         <input
           type="text" name="link" placeholder="http://" value={formData.link} onChange={handleChange}
           style={{
@@ -42,17 +34,8 @@ const Index = () => {
         />
         <input
           type="submit" value="Add Bookmark!"
-          style={{
-            width: '15%',
-            padding: '10px',
-            backgroundColor: '#71b8e4',
-            color: 'black',
-            border: 'none',
-            borderRadius: '5px',
-            fontSize: '16px',
-          }}
-        />
-      </form>
+          style={{width: '15%', padding: '10px', backgroundColor: '#71b8e4', color: 'black', border: 'none', borderRadius: '5px', fontSize: '16px'}}/>
+      </Form>
     </div>
   );
 };
